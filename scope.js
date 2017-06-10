4af39f1c432d6975c9ca050b963073b3e41cbdce
simonyuan100 <simonyuan100@163.com>
2017-06-10 10:45:59 +0800

    作用域链

--- scope.js
+++ scope.js
@@ -13,19 +13,31 @@ var foo = 5;
 /*块作用域和闭包结合*/
 
 for(let i=0;i<5;i++){
-	setTimeout(function(){
-		console.log(i);
-	},200)
+  setTimeout(function(){
+    console.log(i);
+  },200)
 }
 
 /*词法作用域*/
 
+// case 1
 var a = 10
 function foo () {
-	console.log(a)
+  console.log(a)
 }
 function bar () {
-	var a = 5;
-	foo()
+  var a = 5;
+  foo()
 }
 bar() // 10
+
+
+// case 2
+var a = 10
+function bar () {
+  var a = 5;
+  function foo () {
+    console.log(a)
+  }
+}
+bar() // 5
